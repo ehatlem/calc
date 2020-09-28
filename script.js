1c3a080b4643b2359995d8f3dee62098d73a1458
ehatlem <ehatlem@hotmail.com>
2020-09-28 23:51:39 +0200

    Design finished

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 buttonContainer = document.querySelector(".button-container");
+buttonSymbols = ["C", "+/-", "/", 7, 8, 9, "X", 4, 5, 6, "-", 1, 2, 3, "+", 0, ".", "="];
+
 let html = "";
 
-for (let i = 0; i < 16; i++) {
-    html += `<button class="button-item">X</button>`;
+for (let i = 0; i < 18; i++) {
+    if (i === 15 || i === 0) {
+        html += `<button class="button-item button-long">${buttonSymbols[i]}</button>`;
+    } else {
+        html += `<button class="button-item">${buttonSymbols[i]}</button>`;
+    }
+
 }
 
 buttonContainer.innerHTML = html;
+
+buttons = document.querySelectorAll(".button-item");
+
+buttons.forEach(item => {
+
+    item.onclick = function (event) {
+        console.log(item.innerText);
+    }
+});
+
